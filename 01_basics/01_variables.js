73b791e15e953556d289c57a2d6fb88a339135cc
Sarvesh Hadgaonkar <134787311+Sarvesh0820@users.noreply.github.com>
2023-10-05 17:56:29 +0000

    variaables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1423423;
+let userEmail = "sawn@gmil.com";
+var userPassword = 12312;
+userCity = "Mumbai";
+
+// accountId = 3213122 this is not allowed because const is used
+userEmail="fweefw@gmail.com"
+userPassword=21414;
+userCity="Delhi";
+
+console.table([accountId, userEmail, userPassword, userCity]);
+
+// use let instead of var because block scope and functional scope issue
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
